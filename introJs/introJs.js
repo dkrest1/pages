78d6e2959c17688ea4d6e1e12e5061aa9ae36188
Akande Oluwatosin <aktosin1@gmail.com>
2022-06-21 04:30:15 +0100

    completed the introjs task of zuri

---
+++ introJs/introJs.js
@@ -0,0 +1,16 @@
+let myName = "Oluwatosin Akande";
+let height = "5.8ft";
+let country = "Nigeria";
+
+const root = document.getElementById("root");
+
+root.innerHTML = `
+    <div>Name: ${myName}</div>
+    <div>Height: ${height}</div>
+    <div>Country: ${country}</div>
+`;
+
+// console myName, height, country
+console.log(myName);
+console.log(height);
+console.log(country);
